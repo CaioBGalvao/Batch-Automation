350785ee7f8cd691fba3c445b4a2028e0d2610eb
NekuSaga <78922832+NekuSaga@users.noreply.github.com>
2021-02-11 13:40:05 -0300

    1# upload
    Well ... that's how you say it. Hello World.

---
+++ DISM_Manutenção e Gerenciamento de Imagens de Implantação.bat
@@ -0,0 +1,50 @@
+@echo off
+echo Vamos executar o DISM, isso pode demorar MUITO.
+
+echo Nao feche esse Prompt de Comando do meio da execucao.
+
+echo Nao desligue o computador.
+
+echo Esse programa deve ser executado como Administrador.
+
+rem //Verificacao de privilegios
+
+echo Detectando privilegios...
+
+net session >nul 2>&1
+
+if %errorLevel% == 0 (
+
+	rem //Comandos com privilegios de Administrador
+
+	echo Privilegios de Administrador confirmados.
+
+	rem //Captura da unidade
+
+	@echo Exemplo c, d,
+	set /p unidade=Informe a unidade da ISO do Windows: 
+
+	echo A unidade da ISO e '%unidade%:'.
+
+	rem //Dism
+
+	echo Procurando problemas
+
+	DISM.exe /Online /Cleanup-image /Scanhealth
+
+	echo Restaurando saude
+
+	Dism /Online /Cleanup-Image /RestoreHealth /Source:%unidade%:\sources\install.wim
+
+	echo Fim do Programa
+
+	pause >nul
+
+) else (
+
+    rem //Comandos sem privilegios de Administrador
+
+    echo Sem privilegios de Administrador. Feche o Programa e abra como Administrador.
+)
+
+pause >nul
